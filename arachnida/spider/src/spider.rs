9a56e7d575884949a2c024bd6d8edc4c90c51bb5
Jaqensv <matthieu.dl@hotmail.fr>
2025-07-30 16:05:01 +0200

    feat: html scrap +  selector

--- arachnida/spider/src/spider.rs
+++ arachnida/spider/src/spider.rs
@@ -12,14 +12,14 @@ use config::Config;
 mod display;
 use display::display_parsing_error;
 use display::display_values;
-use scraper::Html;
+use scraper::{Html, Selector};
 
 static CONFIG: Lazy<Mutex<Config>> = Lazy::new(|| {
-  Mutex::new(Config {
-    recursive: false,
-    path: String::from("./data/"),
-    depth: 5,
-  })
+	Mutex::new(Config {
+		recursive: false,
+		path: String::from("./data/"),
+		depth: 5,
+	})
 });
 
 fn check_url(url: String) {
@@ -32,49 +32,57 @@ fn check_url(url: String) {
 }
 
 fn create_directory() {
-  let config = CONFIG.lock().unwrap();
-  if let Err(error) = fs::create_dir(config.path.clone()) {
-    eprintln!("Error: failed to create the directory. Reason: {}", error);
-  }
+	let config = CONFIG.lock().unwrap();
+	if let Err(error) = fs::create_dir(config.path.clone()) {
+		eprintln!("Error: failed to create the directory. Reason: {}", error);
+	}
 }
 
 fn parse_options(args: &Vec<String>) {
-  let mut config = CONFIG.lock().unwrap();
-  let mut i = 1;
-  if args.len() > 2 {
-    while i < args.len() {
-      if args[i].starts_with("-r") && args[i].len() == 2 {
-        config.recursive = true;
-      } else if args[i].starts_with("-l") && args[i].len() == 2 && args.get(i + 1).is_some() && args[i + 1].parse::<u32>().is_ok() {
-        config.depth = args[i + 1].parse::<u32>().unwrap();
-        i += 1;
-      } else if args[i].starts_with("-p") && args[i].len() == 2 {
-        if args[i + 1].starts_with("./") {
-          config.path = args[i + 1].clone();
-        }
-        i += 1;
-      } else if args[i].starts_with("http") && i == (args.len() - 1) {
-	      check_url(args[i].clone());
-      } else {
-        display_parsing_error();
-      }
-      i += 1;
-    }
-  } else {
+	let mut config = CONFIG.lock().unwrap();
+	let mut i = 1;
+	if args.len() > 2 {
+		while i < args.len() {
+			if args[i].starts_with("-r") && args[i].len() == 2 {
+				config.recursive = true;
+			} else if args[i].starts_with("-l") && args[i].len() == 2 && args.get(i + 1).is_some() && args[i + 1].parse::<u32>().is_ok() {
+				config.depth = args[i + 1].parse::<u32>().unwrap();
+				i += 1;
+			} else if args[i].starts_with("-p") && args[i].len() == 2 {
+				if args[i + 1].starts_with("./") {
+					config.path = args[i + 1].clone();
+				}
+				i += 1;
+			} else if args[i].starts_with("http") && i == (args.len() - 1) {
+				check_url(args[i].clone());
+			} else {
+				display_parsing_error();
+			}
+			i += 1;
+		}
+	} else {
 		display_parsing_error();
 	}
 }
 
 fn main() {
-  let args: Vec<String> = env::args().collect();
-  parse_options(&args);
-  display_values();
-  create_directory();
-  let client = reqwest::blocking::Client::new();
-  let reponse = client.get(args[args.len() - 1].clone()).header("User-Agent", "Mozilla 5.0").send().expect("Error: request failed");
+	let args: Vec<String> = env::args().collect();
+	parse_options(&args);
+	display_values();
+	create_directory();
+	let client = reqwest::blocking::Client::new();
+	let base_url = Url::parse(&args[args.len() - 1]);
+	//println!("Url is: {:?}", base_url);
+	let reponse = client.get(args[args.len() - 1].clone()).header("User-Agent", "Mozilla 5.0").send().expect("Error: request failed");
 	let body = reponse.text().expect("Request error");
-  let document = Html::parse_document(&body);
-	println!("Url is: {:?}", document);
-
+	let document = Html::parse_document(&body);
+	let selector = Selector::parse("img").unwrap();
+	//println!("Url is: {:?}", selector);
+	//println!("Url is: {:?}", document);
+	for element in document.select(&selector) {
+		let src = element.value().attr("data-src").or_else(|| element.value().attr("src"));
+		let full_url = base_url.clone().expect("Error: failed to create the full_url").join(src.expect("Reason"));
+		println!("Full_url is: {:?}", full_url);
+	}
 }
 
